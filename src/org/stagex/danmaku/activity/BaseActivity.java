65639747311ff4241a48ed8ceb0c74e982017e4f
zhangjie <jwzhangjie@163.com>
2013-08-31 23:09:54 +0800

    add KKTV_HOME--main

---
+++ src/org/stagex/danmaku/activity/BaseActivity.java
@@ -0,0 +1,99 @@
+package org.stagex.danmaku.activity;
+
+import org.keke.player.R;
+
+import net.simonvt.menudrawer.MenuDrawer;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class BaseActivity extends Activity implements OnClickListener {
+
+	private MenuDrawer mMenuDrawer;
+	private float scale;
+	public static final int leftMarge = 70;
+	public static int flag_from = 1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	public void init(int parent, int menu) {
+		mMenuDrawer = MenuDrawer.attach(this, MenuDrawer.MENU_DRAG_WINDOW);
+		mMenuDrawer.setContentView(parent);
+		mMenuDrawer.setMenuView(R.layout.kktv_drawmenu);
+		scale = getResources().getDisplayMetrics().density;
+		mMenuDrawer.setMenuSize(dip2px(leftMarge));
+		initLayout();
+	}
+
+	public void initLayout(){
+		findViewById(R.id.home_first).setOnClickListener(this);
+		findViewById(R.id.home_two).setOnClickListener(this);
+		findViewById(R.id.home_three).setOnClickListener(this);
+		findViewById(R.id.home_four).setOnClickListener(this);
+		findViewById(R.id.home_five).setOnClickListener(this);
+	}
+	public int dip2px(float dpValue) {
+		return (int) (dpValue * scale + 0.5f);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.home_first:
+			if (flag_from != 1) {
+				flag_from = 1;
+				Intent first = new Intent();
+				first.setClass(this, KKTV_HOME.class);
+				startActivity(first);
+			}
+			break;
+		case R.id.home_two:
+			if (flag_from != 2) {
+				flag_from = 2;
+				Intent two = new Intent();
+				two.setClass(this, ChannelTabActivity.class);
+				startActivity(two);
+			}
+			break;
+		case R.id.home_three:
+			if (flag_from != 3) {
+				flag_from = 3;
+				Intent three = new Intent();
+				three.setClass(this, FavouriteActivity.class);
+				startActivity(three);
+			}
+			break;
+		case R.id.home_four:
+			if (flag_from != 4) {
+				flag_from = 4;
+				Intent four = new Intent();
+				four.setClass(this, UserLoadActivity.class);
+				startActivity(four);
+			}
+			break;
+		case R.id.home_five:
+			if (flag_from != 5) {
+				flag_from = 5;
+				Intent five = new Intent();
+				five.setClass(this, SetupActivity.class);
+				startActivity(five);
+			}
+			break;
+		}
+		mMenuDrawer.closeMenu();
+	}
+
+	public void showInfo(int info) {
+		Toast.makeText(this, info, Toast.LENGTH_SHORT).show();
+	}
+
+	public void showInfo(String info) {
+		Toast.makeText(this, info, Toast.LENGTH_SHORT).show();
+	}
+}
